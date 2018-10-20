8273c57ebdcce599aac0554864af678aadfaa8ab
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-20 12:21:52 +0530

    Fixes

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,5 +1,10 @@
-$("img[alt]").each(function(i,a){console.log(a['alt']);});
-$('.input_text').closest('#search').addClass('myform');
-$('#myList>li.current').removeClass('current').next('li').addClass('current');
+$('img[alt]').each(function(index, element) { console.log(element['alt']); });
+$('.input_text').closest('#search')
+  .addClass('myform');
+$('#myList>li.current').removeClass('current')
+  .next('li')
+  .addClass('current');
 $('#specials').find('.input_submit');
-$('#slideshow>li').eq(0).addClass('current').siblings().addClass('disabled');
+$('#slideshow>li').eq(0)
+  .addClass('current')
+  .siblings()
