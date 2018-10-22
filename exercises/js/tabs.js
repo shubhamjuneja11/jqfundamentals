e3eeec5a5c299de728f4ce7c7d86f21152507266
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-22 20:00:58 +0530

    First

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,28 +1,46 @@
-function TabNavigator() {
-
+function TabNavigator(div) {
+  this.div = div
 }
 
 TabNavigator.prototype.init = function() {
-  var modules = $('div.module');
-modules.hide();
+  var modules = $(this.div);
+  modules.hide();
+  this.createTabs(modules);
 };
-var modulesList = $('<div id="moduleslist">');
-modules.each(function() {
-  var $this = $(this);
-  var title = $this.find('h2').html();
-  var id = $this.attr('id');
-  modulesList.append('<li ' + 'data-id=' + id + '>' + title + '</li>');
-});
-console.log(modulesList.html());
-modulesList.on('click', function(event) {
+
+TabNavigator.prototype.createTabs = function(modules) {
+  var modulesList = $('<div>');
+  modules.each(function() {
+    var $this = $(this);
+    var title = $this.find('h2').html();
+    var id = $this.attr('id');
+    modulesList.append('<li ' + 'data-id=' + id + '>' + title + '</li>');
+  });
+  modulesList.insertBefore(modules.first());
+  this.setModuleClickHandler(modules, modulesList);
+  this.selectedModule = modulesList.first();
+  this.selectedModuleId = this.selectedModule.attr('id');
+
+  // Open first tab
+  modulesList.children().first().trigger('click');
+};
+
+TabNavigator.prototype.setModuleClickHandler = function(modules, modulesList) {
+  var _this = this;
+  modulesList.on('click', function(event) {
   var target = event.target;
   var targetId = $(target).attr('data-id');
   $(target).addClass('current');
   modules.siblings('#' + targetId).show();
-  moduleslist.each(function() {
-    $(this).removeClass('current');
-  });
-  //modules.
+
+  // Removing previous selected tab
+  _this.selectedModule.removeClass('current');
+  modules.siblings('#' + _this.selectedModuleId).hide();
+
+  // Setting current selected tab
+  _this.selectedModule = $(target);
+  _this.selectedModuleId = targetId;
 });
-modulesList.insertBefore(modules.first());
-//console.log(modulesList.html());
+}
+var tabNavigator = new TabNavigator('div.module');
+tabNavigator.init();
