d6fdad027f2fa386c36fcbc1612c1481bf642d69
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-22 18:20:24 +0530

    Minor fixes2

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,9 +1,10 @@
 $('img[alt]').each(function(index, element) { console.log(element['alt']); });
-$('.input_text:first').closest('#search')
-                      .addClass('myform');
+$('.input_text').closest('#search')
+                .addClass('myform');
 $('#myList > li.current').removeClass('current')
                          .next('li')
                          .addClass('current');
-$('#specials select').closest('ul').find('.input_submit:first');
-$('#slideshow > li:first').addClass('current')
-                          .siblings();
+$('#specials select').closest('ul')
+                     .find('.input_submit:first');
+$('#slideshow > li').addClass('current')
+                    .siblings();
