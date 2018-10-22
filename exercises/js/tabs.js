01b48dae1ee4be6eec7fe88acf22fd6e985ee70e
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-22 18:18:15 +0530

    Minor fixes2

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,22 +1,28 @@
-var modules = $('div.module');
-//modules.hide();
-//modules.first ().prepend('</ul>')
-var modulesList = $('<div>');
+function TabNavigator() {
+
+}
+
+TabNavigator.prototype.init = function() {
+  var modules = $('div.module');
+modules.hide();
+};
+var modulesList = $('<div id="moduleslist">');
 modules.each(function() {
   var $this = $(this);
   var title = $this.find('h2').html();
   var id = $this.attr('id');
-  // console.log(title);
-  // console.log(id);
   modulesList.append('<li ' + 'data-id=' + id + '>' + title + '</li>');
 });
 console.log(modulesList.html());
 modulesList.on('click', function(event) {
   var target = event.target;
   var targetId = $(target).attr('data-id');
-  console.log(modules.find('#'+targetId));
-  //$this.addClass('current');
+  $(target).addClass('current');
+  modules.siblings('#' + targetId).show();
+  moduleslist.each(function() {
+    $(this).removeClass('current');
+  });
   //modules.
 });
-modules.before(modulesList);
+modulesList.insertBefore(modules.first());
 //console.log(modulesList.html());
