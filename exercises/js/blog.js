5b737dce60ba40e2217ea6a4500499f8f5278793
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-24 02:52:58 +0530

    More fixes

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -13,8 +13,8 @@ BlogToggler.prototype.setToggler = function() {
     var clickedList = $(event.target);
     var selectedList = clickedList.closest('li').find('.excerpt');
     // Open clicked paragraph
-    selectedList.slideDown();
-    if (_this.selectedList) {
+    selectedList.slideToggle();
+    if (_this.selectedList && !_this.compareList(selectedList, _this.selectedList)) {
       // Close previous paragraph
       _this.selectedList.slideUp();
     }
@@ -22,6 +22,15 @@ BlogToggler.prototype.setToggler = function() {
     _this.selectedList = selectedList;
   });
 };
+
+BlogToggler.prototype.compareList = function(list1, list2){
+  return (this.getListName(list1) === this.getListName(list2));
+};
+
+BlogToggler.prototype.getListName = function(list) {
+  return list.siblings('h3').find('a').html();
+};
+
 $(function() {
   var blogToggler = new BlogToggler('#blog');
   blogToggler.init();
