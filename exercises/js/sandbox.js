6d9704119219694aec29e69ea10d26346bb7b6b3
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-23 12:49:34 +0530

    Night is dark and full of terrors

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,11 +1,11 @@
 $('img[alt]').each(function(index, element) { console.log(element['alt']); });
-$('.input_text').closest('#search')
+$('.input_text').parents('form')
                 .addClass('myform');
 $('#myList > li.current').removeClass('current')
                          .next('li')
                          .addClass('current');
 $('#specials select').closest('ul')
-                     .find('.input_submit:first');
+                     .find('input.input_submit');
 $('#slideshow > li:first').addClass('current')
                           .siblings()
                           .addClass('disabled');
