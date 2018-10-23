2f9220ee7461e63ed25b6a386885d8941131c16b
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-23 13:42:24 +0530

    Just do it

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -9,17 +9,25 @@ DropDownMenuHandler.prototype.init = function() {
 DropDownMenuHandler.prototype.setMenuHover = function() {
   var _this = this;
   this.menu.find('li').hover(function(event) {
-    console.log('dddd');
-    var hoveredMenu = $(event.target);
-    console.log(hoveredMenu);
-    hoveredMenu.addClass('selected');
-  },
-  function(event) {
-    var hoveredMenu = $(event.target);
-    hoveredMenu.removeClass('selected');
-  }
+      var hoveredMenu = $(event.target);
+      // Change color of hovered item
+      hoveredMenu.addClass('hover');
+      // Open submenu
+      _this.toggleDropDown(hoveredMenu);
+    },
+    function(event) {
+      var hoveredMenu = $(event.target);
+      hoveredMenu.removeClass('hover');
+      // Close submenu
+      _this.toggleDropDown(hoveredMenu);
+    }
   );
 };
+
+DropDownMenuHandler.prototype.toggleDropDown = function(menuItem) {
+  menuItem.find('ul').toggle(200);
+};
+
 $(function() {
   var dropDownMenu = new DropDownMenuHandler('#nav');
   dropDownMenu.init()
