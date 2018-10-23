dc95c095ab5253a0d6818a6f46ab6bd15ce25948
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-23 13:25:08 +0530

    Back to basics again

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -5,7 +5,7 @@ $('#myList > li.current').removeClass('current')
                          .next('li')
                          .addClass('current');
 $('#specials select').closest('ul')
-                     .find('input.input_submit');
+                     .find('input[type="submit"].input_submit');
 $('#slideshow > li:first').addClass('current')
                           .siblings()
                           .addClass('disabled');
