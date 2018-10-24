3ed60133196bf4c549457596ab13b5e5cd84b02b
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-24 13:17:01 +0530

    More fixes

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,43 +1,43 @@
-function TabNavigator(modulesBlock) {
-  this.modulesBlock = $(modulesBlock);
+function TabNavigator(modulesData) {
+  this.modulesBlock = modulesData.moduleBlock;
 }
 
 TabNavigator.prototype.init = function() {
-  //var modules = $(this.div);
   this.modulesBlock.hide();
   this.createTabs();
+  this.setModuleToggler();
+  this.selectFirstModule();
 };
 
 TabNavigator.prototype.createTabs = function() {
-  var modulesList = $('<div>');
+  var _this = this;
+  this.modulesList = $('<div>');
   this.modulesBlock.each(function() {
-    var $this = $(this);
-    var title = $this.find('h2').html();
-    var id = $this.attr('id');
-    modulesList.append('<li ' + 'data-id=' + id + '>' + title + '</li>');
+    var $this = $(this),
+      title = $this.find('h2').html(),
+      id = $this.attr('id');
+    _this.modulesList.append('<li ' + 'data-id=' + id + '>' + title + '</li>');
   });
-  modulesList.insertBefore(this.modulesBlock.first());
-  this.modulesList = modulesList;
-  this.setModuleToggler();
-  this.selectedModule = modulesList.first();
-  this.selectedModuleId = this.selectedModule.attr('id');
+  this.modulesList.insertBefore(this.modulesBlock.first());
+};
 
-  // Open first tab
-  modulesList.children().first().trigger('click');
+TabNavigator.prototype.selectFirstModule = function() {
+  this.modulesList.children().first().trigger('click');
 };
 
 TabNavigator.prototype.setModuleToggler = function() {
   var _this = this;
   this.modulesList.on('click', 'li', function(event) {
-    var target = $(this);
-    var targetId = target.attr('data-id');
+    var target = $(this),
+      targetId = target.attr('data-id');
     target.addClass('current');
     _this.modulesBlock.siblings('#' + targetId).show();
 
     // Removing previous selected tab
+    if(_this.selectedModule) {
     _this.selectedModule.removeClass('current');
     _this.modulesBlock.siblings('#' + _this.selectedModuleId).hide();
-
+}
     // Setting current selected tab
     _this.selectedModule = target;
     _this.selectedModuleId = targetId;
@@ -45,6 +45,9 @@ TabNavigator.prototype.setModuleToggler = function() {
 }
 
 $(function() {
-  var tabNavigator = new TabNavigator('div.module');
+  var options = {
+    moduleBlock: $('div.module')
+  };
+  var tabNavigator = new TabNavigator(options);
   tabNavigator.init();
 });
